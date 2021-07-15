7b363bc0ffca5ded66e6edf92771416a5d40a42b
Ditipriya Seal <67802174+Ditipriya5678@users.noreply.github.com>
2021-07-15 22:23:29 +0530

    Create Dynamic-memory_malloc.c

---
+++ Dynamic-memory_malloc.c
@@ -0,0 +1,28 @@
+//Dynamic memory allocation using malloc() in C
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int n,i;
+	printf("Enter number of integers: ");
+	scanf("%d",&n);
+	int *ptr = (int *)malloc(n*sizeof(int));  //memory allocation for n integers
+	printf("Enter the integers:\n");
+	if(ptr==NULL)
+	{
+		printf("Memory not available.\n");
+		exit(1);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d", (ptr+i));
+	}
+	printf("The integers are:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\n", *(ptr+i));
+	}
+	return 0;
+}
